ca2b798564403c1525a82e1a828aae2702d6b31a
NQB37 <nqb37.dev@gmail.com>
2026-03-15 18:53:32 +0100

    feat: setup tanstack query provider

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/shared/theme-provider';
+import QueryProvider from '@/components/providers/query-provider';
 import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
@@ -36,8 +37,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className='flex-1'>{children}</main>
-          <Toaster />
+          <QueryProvider>
+            <main className='flex-1'>{children}</main>
+            <Toaster />
+          </QueryProvider>
         </ThemeProvider>
       </body>
     </html>
